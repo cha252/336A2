b28c89024cc5a20e1d7c5ab9c8f82d9f37046610
Chisora <chisorah15092004@gmail.com>
2025-09-16 21:15:56 +1200

    Working with right cache type and now reads images from phone's camera roll

--- app/src/main/java/com/example/hada_a2/MainActivity.kt
+++ app/src/main/java/com/example/hada_a2/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.hada_a2
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,83 +20,147 @@ import androidx.compose.ui.unit.dp
 import com.example.hada_a2.ui.theme.Hada_A2Theme
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.collection.LruCache
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlinx.serialization.internal.throwMissingFieldException
 
 class MainActivity : ComponentActivity() {
-    //List of images' ids
-    val imageIds = listOf(
-        R.drawable.rome,
-        R.drawable.ansel,
-        R.drawable.kauai,
-        R.drawable.despair,
-        R.drawable.adventure_beautiful_daylight_325807,
-        R.drawable.agriculture_color_cooking_255501,
-        R.drawable.ancient_architecture_birds_819806,
-        R.drawable.apartments_architecture_bay_632522,
-        R.drawable.architecture_autumn_bench_206673,
-        R.drawable.arid_barren_black_and_white_150944,
-        R.drawable.autumn_autumn_leaves_fall_1563355,
-        R.drawable.background_biker_clouds_207171,
-        R.drawable.beach_bora_bora_clouds_753626,
-        R.drawable.beach_clouds_daytime_994605,
-        R.drawable.beach_clouds_dusk_783725,
-        R.drawable.caimanphotography3,
-        R.drawable.coast_dusk_evening_1048273,
-        R.drawable.daylight_forest_glossy_443446,
-        R.drawable.eagle_fall_sunrise,
-        R.drawable.longue_vue,
-        R.drawable.mauisunset,
-        R.drawable.mona_big,
-        R.drawable.one_wheel,
-        R.drawable.rubihorn,
-        R.drawable.train_wreck_at_montparnasse_1895,
-        R.drawable.yosemite_tree
-    )
+    //Declare variables
+    private var hasPermission by mutableStateOf(false)
+    private lateinit var memoryCache: LruCache<String, Bitmap>
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        hasPermission = isGranted
+    }
 
-    @Composable
-    //Lazy vertical grid to display the images
-    fun CameraRoll(modifier: Modifier = Modifier) {
-        val context = LocalContext.current
-
-        //Lazy vertical grid to display images
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            contentPadding = PaddingValues(8.dp),
-            modifier = modifier.fillMaxSize()
-        ) {
-            items(imageIds.size) { index ->
-                ThumbnailImage(imageIds[index])
+    //Function to request appropriate permission based on device
+    private fun checkAndRequestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+            }
+            else{
+                hasPermission = true
+            }
+        } else {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+            else{
+                hasPermission = true
             }
         }
     }
 
+    //Function to get images from device memory
+    fun getImages(context: Context): List<Uri>{
+        //Declare variables
+        val uris = mutableListOf<Uri>()
+        val collection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val projectionn = arrayOf(MediaStore.Images.Media._ID)
+
+        //Add all images to uris in date order (newest first)
+        context.contentResolver.query(
+            collection,
+            projectionn,
+            null,
+            null,
+            "${MediaStore.Images.Media.DATE_ADDED} DESC"
+        )?.use{ cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+
+            while(cursor.moveToNext()){
+                val id = cursor.getLong(idColumn)
+                val contentUri = Uri.withAppendedPath(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id.toString()
+                )
+                uris.add(contentUri)
+            }
+        }
+
+        return uris
+    }
+
+    //Lazy vertical grid to display the images
+    @Composable
+    fun CameraRoll(modifier: Modifier = Modifier) {
+        //Declare variables
+        val context = LocalContext.current
+        var imageUris by remember { mutableStateOf<List<Uri>>(emptyList()) }
+
+        //Check for permission
+        LaunchedEffect(hasPermission) { if(hasPermission) { imageUris = getImages(context) } }
+
+        //If the app has the required permission
+        if(hasPermission) {
+            //Set the lazy vertical grid
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(8.dp),
+                modifier = modifier.fillMaxSize()
+            ) {
+                //Set each item of the grid as a thumbnail
+                items(imageUris.size) { index ->
+                    ThumbnailImage(imageUris[index])
+                }
+            }
+        }
+        //If permission has not been granted
+        else {
+            //Show some text to say that the permission is required
+            Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("Permission required to show images")
+            }
+        }
+    }
+
+
     @Composable
     //Thumbnail function to get a thumbnail of each image
-    fun ThumbnailImage(imageId: Int) {
+    fun ThumbnailImage(uri: Uri) {
         //Declare variables
         val context = LocalContext.current
         var bitmap by remember { mutableStateOf<Bitmap?>(null) }
 
-        //Use a coroutine to get the thumbnail
-        LaunchedEffect(imageId) {
-            withContext(Dispatchers.IO) {
-                try {
-                    val original = BitmapFactory.decodeResource(context.resources, imageId)
-                    val thumbnail = Bitmap.createScaledBitmap(original, 400, 400, true)
-                    bitmap = thumbnail
-                } catch (e: Exception) {
-                    e.printStackTrace()
+        LaunchedEffect(uri) {
+            //Use the uri string as the key
+            val key = uri.toString()
+
+            //Check if in cache first
+            val cached = (context as MainActivity).memoryCache.get(key)
+            //If the image has already been cached
+            if(cached != null){
+                //Set bitmap to the cached image
+                bitmap = cached
+            }
+            else {
+                //Use a coroutine to get the thumbnail
+                withContext(Dispatchers.IO) {
+                    try {
+                        context.contentResolver.openInputStream(uri)?.use{stream ->
+                            val original = BitmapFactory.decodeStream(stream)
+                            val thumbnail = Bitmap.createScaledBitmap(original, 400, 400, true)
+                            bitmap = thumbnail
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
                 }
             }
         }
@@ -120,6 +187,18 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Set up cache size
+        val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
+        val cacheSize = maxMemory / 8
+        memoryCache = object: LruCache<String, Bitmap>(cacheSize){
+            override fun sizeOf(key: String, value: Bitmap): Int {
+                return value.byteCount / 1024
+            }
+        }
+        //Check for the permission and get if required
+        checkAndRequestPermission()
+
         enableEdgeToEdge()
         setContent {
             Hada_A2Theme {
